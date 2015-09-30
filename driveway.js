33d85398cee9bcb7d853c767877cd3a9a25e2a89
Student <student@Mac-mini-31.local>
2015-09-30 15:16:33 -0600

    fixed the info window, now need to style it more

--- driveway.js
+++ driveway.js
@@ -61,23 +61,14 @@ var drivewayControllerFunc = function ($scope) {
       				var contentString = '<div id="content">'+
       					'<div id="siteNotice">'+
       					'</div>'+
-      					'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      					'<div id="bodyContent">'+
-      					'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      					'<div id="bodyContent">' + 
+      					'<p><b>' + $scope.street + '</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
       					'sandstone rock formation in the southern part of the '+
-      					'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      					'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      					'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      					'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      					'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      					'Aboriginal people of the area. It has many springs, waterholes, '+
-      					'rock caves and ancient paintings. Uluru is listed as a World '+
       					'Heritage Site.</p>'+
-      					'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      					'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      					'(last visited June 22, 2009).</p>'+
+      					'<img src="curbtopark.png"'+
       					'</div>'+
       					'</div>';
+
 
   					var infowindow = new google.maps.InfoWindow({
     						content: contentString,
@@ -96,7 +87,7 @@ var drivewayControllerFunc = function ($scope) {
     				clickedMarkerTitle.setMap(null); 			
     			});
     						
-    			google.maps.event.addListener(marker, 'mouseover', function(event) {
+    			google.maps.event.addListener(marker, 'click', function(event) {
        		 		infowindow.open(map, marker);
     						});
 
