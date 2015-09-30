8dffb95815f3ce91ec00e1e85fba09fac5f38f83
Student <student@Mac-mini-31.local>
2015-09-30 14:58:55 -0600

    added info window and have not styled it yet

--- driveway.js
+++ driveway.js
@@ -39,7 +39,8 @@ var drivewayControllerFunc = function ($scope) {
 	$scope.Submit = function () {
 		$scope.showaddressform = false;
 		$scope.showmap = true;
-		var image = 'redcaricon.png';
+		var parkimage = 'curbtopark.png';
+		var image = 'blackcar.png';
 		$scope.address = $scope.street + " " + $scope.city + " " + $scope.state + " " + $scope.zip;
 
 		var geocoder = new google.maps.Geocoder();
@@ -57,6 +58,32 @@ var drivewayControllerFunc = function ($scope) {
 
       				})
 
+      				var contentString = '<div id="content">'+
+      					'<div id="siteNotice">'+
+      					'</div>'+
+      					'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      					'<div id="bodyContent">'+
+      					'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      					'sandstone rock formation in the southern part of the '+
+      					'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      					'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      					'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      					'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      					'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      					'Aboriginal people of the area. It has many springs, waterholes, '+
+      					'rock caves and ancient paintings. Uluru is listed as a World '+
+      					'Heritage Site.</p>'+
+      					'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      					'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      					'(last visited June 22, 2009).</p>'+
+      					'</div>'+
+      					'</div>';
+
+  					var infowindow = new google.maps.InfoWindow({
+    						content: contentString,
+    						maxWidth: 200
+  						});
+
 
 
 				google.maps.event.addListener(marker, 'click', function(event) {
@@ -70,7 +97,7 @@ var drivewayControllerFunc = function ($scope) {
     			});
     						
     			google.maps.event.addListener(marker, 'mouseover', function(event) {
-       		 		console.log(marker.title)
+       		 		infowindow.open(map, marker);
     						});
 
 
